eead2afde6cb4f699192c4c493eb21c7096391e8
Cesar Adrian Mahecha Marin <camahecha96@misena.edu.co>
2018-04-20 13:47:53 -0500

    My first job php
    First php exercise with my name and message

---
+++ Planeación/php/myname/index.php
@@ -0,0 +1,10 @@
+
+        <?php
+        require 'name.php';
+        use myname\name;
+        $name = new name();
+        $name -> myname();
+        // ingreso la siguiente variable
+        $programing= new name();
+        $programing ->oop();
+        ?>
---
+++ Planeación/php/myname/name.php
@@ -0,0 +1,14 @@
+<?php
+namespace myname;
+
+class name{
+    
+    public function myname (){
+        echo 'Cesar Adrian Mahecha Marin.';
+        echo "<br>";
+    }
+    public function oop (){
+        echo 'This is my second message in PHP through oject-oriented-programing.';
+    }
+}
+
